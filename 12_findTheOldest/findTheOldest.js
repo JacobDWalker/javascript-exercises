9d080ef64172af009ac1ff83e1b2c7c44ba784f8
JacobDWalker <jacobdanwalker@gmail.com>
2022-07-14 21:14:43 -0400

    Complete problems

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
 const findTheOldest = function(array) {
 
-
+    return array.reduce((oldest, currentPerson) => {
+        let oldestAge = getAge(oldest);
+        let personAge = getAge(currentPerson);
+        return personAge > oldestAge ? currentPerson : oldest;
+    })
 }
+
+function getAge(person) {
+    if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
+
+
 // Do not edit below this line
 module.exports = findTheOldest;
+
